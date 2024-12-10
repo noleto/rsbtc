046efcf5c8df7effde4f13ca31fa1697ef02fcdb
Leo Noleto <noleto.leonardo@gmail.com>
2024-12-10 23:19:06 +0100

    creating common lib.

---
+++ lib/src/lib.rs
@@ -0,0 +1,12 @@
+use uint::construct_uint;
+
+construct_uint! {
+    // Construct an unsigned 256-bit integer
+    // consisting of 4 x 64-bit words
+    pub struct U256(4);
+}
+
+pub mod crypto;
+pub mod sha256;
+pub mod types;
+pub mod util;
